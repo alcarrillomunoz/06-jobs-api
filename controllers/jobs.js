826949bce4278db3697aea2d46d1634127a5e051
Lily <alcarrillomunoz@gmail.com>
2025-01-30 14:29:54 -0500

    A Front End for the Jobs API

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,55 +1,69 @@
-const Job = require('../models/Job');
-const { StatusCodes } = require('http-status-codes');
-const { BadRequestError, NotFoundError } = require('../errors');
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors");
 
 const getAllJobs = async (req, res) => {
-    const jobs = await Job.find({ createdBy:req.user.userId }).sort('createdAt');
-    res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
-}
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
+};
 
 const getJob = async (req, res) => {
-    const { user: {userId}, params: {id:jobId} } = req;
-    const job = await Job.findOne({
-        _id:jobId,
-        createdBy:userId
-    })
-    if (!job) {
-        throw new NotFoundError(`No job with id: ${jobId}`);
-    }
-    res.status(StatusCodes.OK).json({ job });
-}
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  const job = await Job.findOne({
+    _id: jobId,
+    createdBy: userId,
+  });
+  if (!job) {
+    throw new NotFoundError(`No job with id: ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
+};
 const createJob = async (req, res) => {
-    req.body.createdBy = req.user.userId;
-    const job = await Job.create(req.body);
-    res.status(StatusCodes.CREATED).json({ job });
-}
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
+};
 
 const updateJob = async (req, res) => {
-    const { body:{company, position}, user: {userId}, params: {id:jobId} } = req;
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: jobId },
+  } = req;
 
-    if (company === '' || position === '') {
-        throw new BadRequestError('Please provide a company and position');
-    }
+  if (company === "" || position === "") {
+    throw new BadRequestError("Please provide a company and position");
+  }
 
-    const job = await Job.findByIdAndUpdate({ _id:jobId, createdBy:userId }, req.body, {new:true, runValidators:true});
-    
-    if (!job) {
-        throw new NotFoundError(`No job with id ${jobId}`);
-    }
+  const job = await Job.findByIdAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
 
-    res.status(StatusCodes.OK).json({ job }); 
-}
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+
+  res.status(StatusCodes.OK).json({ job });
+};
 
 const deleteJob = async (req, res) => {
-    const { user: {userId}, params: {id: jobId}, } = req; 
-    
-    const job = await Job.findByIdAndDelete({ _id:jobId, createdBy:userId });
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
 
-    if (!job) {
-        throw new NotFoundError(`No job with id ${ jobId }`);
-    }
+  const job = await Job.findByIdAndDelete({ _id: jobId, createdBy: userId });
 
-    res.status(StatusCodes.OK).send(); 
-}
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
 
-module.exports = { getAllJobs, getJob, createJob, updateJob, deleteJob }
+  res.status(StatusCodes.OK).json({ msg: "The entry was deleted." });
+};
+
+module.exports = { getAllJobs, getJob, createJob, updateJob, deleteJob };
